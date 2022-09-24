6c6793a4e6396cca85d60e743c097487e13b3ca8
Yury Samuel <Yuryh084@gmail.com>
2022-09-24 13:34:03 -0300

    Update Q1.java

--- ATIVIDADE03/src/ATIVIDADE/Q1.java
+++ ATIVIDADE03/src/ATIVIDADE/Q1.java
@@ -8,18 +8,27 @@ import java.util.Stack;
 
 public class Q1 {
 
-	public static void main(String[] args) throws FileNotFoundException{
-		Scanner scanner;
-		Stack pilha = new Stack(); // Classe Pilha
-		
-        scanner = new Scanner(new File("caminho do arquivo"));
-        while (scanner.hasNextLine()){
-            pilha.push(scanner.nextLine());
-        }
-       System.out.println("Pilha de Nomes");
-        while(!pilha.empty())
-		   {
-		        System.out.println(pilha.pop());
-		    }
-	}
+	  public static void main(String[] args) throws FileNotFoundException {
+        Stack<String> frases = new Stack<>();
+        Queue<String> top10Frases = new LinkedList<>();
+        Scanner scanner;
+
+        scanner = new Scanner(new File("./frases.txt"));
+        while (scanner.hasNextLine())
+            frases.add(scanner.nextLine());
+
+        if (!frases.isEmpty()) {
+            while (top10Frases.size() < 10)
+                top10Frases.add(frases.pop());
+
+            System.out.println("Frases depois da decima:");
+            while (!frases.empty())
+                System.out.println(frases.pop());
+
+            System.out.println("-----------------------------------");
+            System.out.println("Top 10 frases:");
+            top10Frases.forEach(System.out::println);
+        } else
+            System.out.println("Não existem frases buscadas.");
+    }
 }
